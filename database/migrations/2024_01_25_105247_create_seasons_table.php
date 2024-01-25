44ae686f33d3fe36db4aeafc1b07815f61930f22
Seyed <seyeedm0@gmail.com>
2024-01-25 15:42:27 +0330

    Start Project

---
+++ database/migrations/2024_01_25_105247_create_seasons_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("image");
+            $table->text("description");
+            $table->integer("price");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign('user_id')->references('id')->on('users')->onDelete("cascade");
+            $table->timestamps();
+        });
+
+        Schema::create('seasons', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->integer("count_video");
+            $table->unsignedBigInteger("course_id");
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+        Schema::dropIfExists('seasons');
+    }
+};
